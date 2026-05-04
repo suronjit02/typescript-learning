e688eb3a36b4e6b689162b1034e186afec0c4503
Suronjit Sutradhar <suronjit02@gmail.com>
2026-05-04 17:07:13 +0600

    practice enum

---
+++ src/enum.ts
@@ -0,0 +1,19 @@
+//  enum : set of fixed string literal same place
+
+// type UserRoles = "Admin" | "Editor" | "Viewer";
+
+enum UserRoles {
+  Admin = "Admin",
+  Editor = "Editor",
+  Viewer = "Viewer",
+}
+
+const canEdit = (role: UserRoles) => {
+  if (role === UserRoles.Admin || role === UserRoles.Editor) {
+    return true;
+  } else return false;
+};
+
+const isEditPermissible = canEdit(UserRoles.Admin);
+
+console.log(isEditPermissible);
