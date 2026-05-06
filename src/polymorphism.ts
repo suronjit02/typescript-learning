e65c3e901bd32465a07916b9ec8ae7fe67f784a4
Suronjit Sutradhar <suronjit02@gmail.com>
2026-05-06 17:49:31 +0600

    explore and practice polymorphism

---
+++ src/polymorphism.ts
@@ -0,0 +1,78 @@
+// polymorphism : bohurupi
+
+class Person {
+  getSleep() {
+    console.log(`I am a Normal Person. I sleep for 8 Hours`);
+  }
+}
+
+class Student extends Person {
+  getSleep() {
+    console.log(`I am a student. I sleep for 7 hours`);
+  }
+}
+class NextLevelDeveloper extends Person {
+  getSleep() {
+    console.log(`I am a Next Level Developer. I sleep for 6 hours`);
+  }
+}
+
+const getSleepingHours = (param: Person) => {
+  param.getSleep();
+};
+
+const person1 = new Person();
+const person2 = new Student();
+const person3 = new NextLevelDeveloper();
+
+getSleepingHours(person2);
+
+// --------------------------------------------------------------------
+
+class Shape {
+  getArea(): number {
+    return 0;
+  }
+}
+
+class Circle extends Shape {
+  // area = pi*r*r
+  radius: number;
+
+  constructor(radius: number) {
+    super();
+    this.radius = radius;
+  }
+
+  getArea(): number {
+    return Math.PI * this.radius * this.radius;
+  }
+}
+
+class Rectangle extends Shape {
+  // area = height * width
+
+  height: number;
+  width: number;
+
+  constructor(height: number, width: number) {
+    super();
+
+    this.height = height;
+    this.width = width;
+  }
+
+  getArea(): number {
+    return this.height * this.width;
+  }
+}
+
+const getArea = (param: Shape) => {
+  console.log(param.getArea());
+};
+
+const shape1 = new Shape();
+const shape2 = new Circle(10);
+const shape3 = new Rectangle(10, 20);
+
+getArea(shape3);
